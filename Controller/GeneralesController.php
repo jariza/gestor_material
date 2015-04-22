cb1428dc9d950d186353d81aff97362a9bb8eedc
José Jaime Ariza <jariza@ieee.org>
2015-04-23 01:14:44 +0200

    Añadida verificación global de estado y corregidos algunos errores.

--- Controller/GeneralesController.php
+++ Controller/GeneralesController.php
@@ -31,13 +31,13 @@ class GeneralesController extends AppController {
 		else {
 			$this->set('imprimir', false);
 		}
-		
+
 		$mnecesidadactividad = $this->loadModel('Necesidadactividad');
 		$mnecesidadzona = $this->loadModel('Necesidadzona');
 		$mzona = $this->loadModel('Zona');
 		$neczona = $this->Necesidadzona->find('all', array('conditions' => array('infraestructura' => true), 'fields' => array('Necesidadzona.descripcion', 'Necesidadzona.cantidad', 'Zona.nombre')));
 		$necactividad = $this->Necesidadactividad->find('all', array('conditions' => array('infraestructura' => true), 'fields' => array('Necesidadactividad.descripcion', 'Necesidadactividad.cantidad', 'Actividad.nombre', 'Actividad.zona_id')));
-		
+
 		$zonaid = array();
 		foreach ($necactividad as $v) {
 			$zonaid[] = $v['Actividad']['zona_id'];
@@ -47,9 +47,202 @@ class GeneralesController extends AppController {
 		foreach ($results as $v) {
 			$zonas[$v['Zona']['id']] = $v['Zona']['nombre'];
 		}
-		
+
 		$this->set('neczona', $neczona);
 		$this->set('necactividad', $necactividad);
 		$this->set('zonas', $zonas);
+	}
+
+	public function estado() {
+		$mnecesidadzona = $this->loadModel('Necesidadzona');
+		$mnecesidadactividad = $this->loadModel('Necesidadactividad');
+
+		//No satisfechas
+		$this->Necesidadactividad->bindModel(array(
+			'hasOne' => array('Zona' => array('foreignKey' => false, 'conditions' => 'Actividad.zona_id = Zona.id'))
+		));
+		$nosatzona = $this->Necesidadzona->find('all', array('conditions' => array('infraestructura' => 0, 'objeto_id' => null), 'fields' => array('Necesidadzona.cantidad', 'Necesidadzona.descripcion', 'Zona.nombre', 'Zona.id'), 'order' => 'Zona.nombre'));
+		$nosatactividad = $this->Necesidadactividad->find('all', array('conditions' => array('infraestructura' => 0, 'objeto_id' => null), 'fields' => array('Necesidadactividad.cantidad', 'Necesidadactividad.cantidad', 'Necesidadactividad.descripcion', 'Zona.nombre', 'Actividad.nombre', 'Zona.id', 'Actividad.id'), 'order' => array('Zona.nombre', 'Actividad.nombre')));
+
+		//Objetos asignados a zonas
+		$this->Necesidadactividad->bindModel(array(
+			'hasOne' => array('Zona' => array('foreignKey' => false, 'conditions' => 'Actividad.zona_id = Zona.id'))
+		));
+		$results1 = $this->Necesidadzona->find('all', array('conditions' => array('infraestructura' => 1, 'objeto_id !=' => null), 'fields' => array('Necesidadzona.cantidad', 'Necesidadzona.descripcion', 'Zona.nombre', 'Zona.id', 'Objeto.descripcion', 'Objeto.id'), 'order' => 'Zona.nombre'));
+		$results2 = $this->Necesidadactividad->find('all', array('conditions' => array('infraestructura' => 1, 'objeto_id !=' => null), 'fields' => array('Necesidadactividad.cantidad', 'Necesidadactividad.cantidad', 'Necesidadactividad.descripcion', 'Zona.nombre', 'Zona.id', 'Actividad.nombre', 'Actividad.id', 'Objeto.descripcion', 'Objeto.id'), 'order' => array('Zona.nombre', 'Actividad.nombre')));
+		$infraobjeto = array();
+		foreach ($results1 as $v) {
+			$infraobjeto[] = array(
+				'nomzona' => $v['Zona']['nombre'],
+				'idzona' => $v['Zona']['id'],
+				'nomactividad' => '',
+				'idactividad' => -1,
+				'descobjeto' => $v['Objeto']['descripcion'],
+				'idobjeto' => $v['Objeto']['id'],
+				'necesidad' => $v['Necesidadzona']['descripcion'],
+				'cantidad' => $v['Necesidadzona']['cantidad']
+			);
+		}
+		foreach ($results2 as $v) {
+			$infraobjeto[] = array(
+				'nomzona' => $v['Zona']['nombre'],
+				'idzona' => $v['Zona']['id'],
+				'nomactividad' => $v['Actividad']['nombre'],
+				'idactividad' => $v['Actividad']['id'],
+				'descobjeto' => $v['Objeto']['descripcion'],
+				'idobjeto' => $v['Objeto']['id'],
+				'necesidad' => $v['Necesidadactividad']['descripcion'],
+				'cantidad' => $v['Necesidadactividad']['cantidad']
+			);
+		}
+
+		//No fungible con cantidad superior a uno
+		$this->Necesidadactividad->bindModel(array(
+			'hasOne' => array('Zona' => array('foreignKey' => false, 'conditions' => 'Actividad.zona_id = Zona.id'))
+		));
+		$results1 = $this->Necesidadzona->find('all', array('conditions' => array('Objeto.fungible' => 0, 'Necesidadzona.cantidad >' => 1), 'fields' => array('Necesidadzona.cantidad', 'Necesidadzona.descripcion', 'Zona.nombre', 'Zona.id', 'Objeto.descripcion', 'Objeto.id'), 'order' => 'Zona.nombre'));
+		$results2 = $this->Necesidadactividad->find('all', array('conditions' => array('Objeto.fungible' => 0, 'Necesidadactividad.cantidad >' => 1), 'fields' => array('Necesidadactividad.cantidad', 'Necesidadactividad.cantidad', 'Necesidadactividad.descripcion', 'Zona.nombre', 'Zona.id', 'Actividad.nombre', 'Actividad.id', 'Objeto.descripcion', 'Objeto.id'), 'order' => array('Zona.nombre', 'Actividad.nombre')));
+		$multiobjeto = array();
+		foreach ($results1 as $v) {
+			$multiobjeto[] = array(
+				'nomzona' => $v['Zona']['nombre'],
+				'idzona' => $v['Zona']['id'],
+				'nomactividad' => '',
+				'idactividad' => -1,
+				'descobjeto' => $v['Objeto']['descripcion'],
+				'idobjeto' => $v['Objeto']['id'],
+				'necesidad' => $v['Necesidadzona']['descripcion'],
+				'cantidad' => $v['Necesidadzona']['cantidad']
+			);
+		}
+		foreach ($results2 as $v) {
+			$multiobjeto[] = array(
+				'nomzona' => $v['Zona']['nombre'],
+				'idzona' => $v['Zona']['id'],
+				'nomactividad' => $v['Actividad']['nombre'],
+				'idactividad' => $v['Actividad']['id'],
+				'descobjeto' => $v['Objeto']['descripcion'],
+				'idobjeto' => $v['Objeto']['id'],
+				'necesidad' => $v['Necesidadactividad']['descripcion'],
+				'cantidad' => $v['Necesidadactividad']['cantidad']
+			);
+		}
+
+		//Sobreasignación de fungibles
+		$this->Necesidadactividad->unbindModel(array('belongsTo' => 'Actividad')); //Ahorro SQL
+		$this->Necesidadzona->virtualFields['uso'] = 'SUM(Necesidadzona.cantidad)';
+		$results1 = $this->Necesidadzona->find('all', array('fields' => array('Necesidadzona.objeto_id', 'uso', 'Objeto.descripcion', 'Objeto.id', 'Objeto.cantidad'), 'conditions' => array('Objeto.fungible' => 1), 'group' => array('objeto_id')));
+		$this->Necesidadactividad->virtualFields['uso'] = 'SUM(Necesidadactividad.cantidad)';
+		$results2 = $this->Necesidadactividad->find('all', array('fields' => array('Necesidadactividad.objeto_id', 'uso', 'Objeto.descripcion', 'Objeto.id', 'Objeto.cantidad'), 'conditions' => array('Objeto.fungible' => 1), 'group' => array('objeto_id')));
+		$sobreasignacion = array();
+		foreach ($results1 as $v) {
+			$sobreasignacion[$v['Necesidadzona']['objeto_id']] = array(
+				'uso' => $v['Necesidadzona']['uso'],
+				'descobjeto' => $v['Objeto']['descripcion'],
+				'idobjeto' => $v['Objeto']['id'],
+				'disponible' => $v['Objeto']['cantidad']
+			);
+		}
+		foreach ($results2 as $v) {
+			if (array_key_exists($v['Necesidadactividad']['objeto_id'], $sobreasignacion)) {
+				$sobreasignacion[$v['Necesidadactividad']['objeto_id']]['uso'] += $v['Necesidadactividad']['uso'];
+			}
+			else {
+				$sobreasignacion[$v['Necesidadactividad']['objeto_id']] = array(
+					'uso' => $v['Necesidadactividad']['uso'],
+					'descobjeto' => $v['Objeto']['descripcion'],
+					'idobjeto' => $v['Objeto']['id'],
+					'disponible' => $v['Objeto']['cantidad']
+				);
+			}
+		}
+		//Elimino los no sobreasignados
+		foreach ($sobreasignacion as $k => $v) {
+			if ($v['uso'] <= $v['disponible']) {
+				unset($sobreasignacion[$k]);
+			}
+		}
+
+		//Objetos usados en zonas y actividades
+		$this->Necesidadactividad->bindModel(array(
+			'hasOne' => array(
+				'Necesidadzona' => array('foreignKey' => false, 'type' => 'INNER', 'conditions' => array('Necesidadzona.objeto_id = Necesidadactividad.objeto_id', 'Objeto.fungible' => 0)),
+				'Zona' => array('foreignKey' => false, 'conditions' => 'Necesidadzona.zona_id = Zona.id')
+			)
+		));
+		$results = $this->Necesidadactividad->find('all', array('fields' => array('Objeto.descripcion', 'Objeto.id', 'Actividad.nombre', 'Actividad.id', 'Zona.nombre', 'Zona.id')));
+		$activzona = array();
+		foreach ($results as $v) {
+			$activzona[$v['Objeto']['id']]['descripcion'] = $v['Objeto']['descripcion'];
+			$activzona[$v['Objeto']['id']]['zonas'][$v['Zona']['id']] = $v['Zona']['nombre'];
+			$activzona[$v['Objeto']['id']]['actividades'][$v['Actividad']['id']] = $v['Actividad']['nombre'];
+		}
+
+		//Obtener necesidades y sus horas
+		//Los que son de alguna sesión
+		$this->Necesidadactividad->unbindModel(array('hasOne' => array('Necesidadzona', 'Zona')));
+		$this->Necesidadactividad->bindModel(
+			array('hasOne' => array(
+				'Horario' => array('foreignKey' => false, 'type' => 'INNER', 'conditions' => array('Horario.actividad_id = Necesidadactividad.actividad_id', 'Horario.sesion = Necesidadactividad.sesion', 'Necesidadactividad.sesion != ' => 0, 'Necesidadactividad.objeto_id !=' => null))
+				)
+			)
+		);
+		$results1 = $this->Necesidadactividad->find('all', array('conditions' => array('Necesidadactividad.infraestructura' => 0, 'Objeto.fungible' => 0), 'fields' => array('Actividad.id', 'Actividad.nombre', 'Objeto.id', 'Objeto.descripcion', 'Horario.inicio', 'Horario.fin')));
+		//Los que son de todas las sesiones
+		$this->Necesidadactividad->unbindModel(array('hasOne' => array('Horario')));
+		$this->Necesidadactividad->bindModel(
+			array('hasOne' => array(
+				'Horario' => array('foreignKey' => false, 'type' => 'INNER', 'conditions' => array('Horario.actividad_id = Necesidadactividad.actividad_id', 'Necesidadactividad.sesion' => 0, 'Necesidadactividad.objeto_id !=' => null))
+				)
+			)
+		);
+		$results2 = $this->Necesidadactividad->find('all', array('conditions' => array('Necesidadactividad.infraestructura' => 0, 'Objeto.fungible' => 0), 'fields' => array('Actividad.id', 'Actividad.nombre', 'Objeto.id', 'Objeto.descripcion', 'Horario.inicio', 'Horario.fin')));
+		//Montar el horario
+		$results = array_merge($results1, $results2);
+		$horarioobjetos = array(); //El key es el id de objeto
+		foreach ($results as $v) {
+			$horarioobjetos[$v['Objeto']['id']]['descobjeto'] = $v['Objeto']['descripcion'];
+			$horarioobjetos[$v['Objeto']['id']]['horarios'][] = array(
+				'nomactividad' => $v['Actividad']['nombre'],
+				'idactividad' => $v['Actividad']['id'],
+				'inicio' => $v['Horario']['inicio'],
+				'fin' => $v['Horario']['fin'],
+				'solapado' => false
+			);
+		}
+		//Comprobar solapes
+		foreach (array_keys($horarioobjetos) as $k) {
+			for ($i = 0; $i < count($horarioobjetos[$k]['horarios']); $i++) {
+				for ($j = $i+1; $j < count($horarioobjetos[$k]['horarios']); $j++) {
+					if (($horarioobjetos[$k]['horarios'][$i]['fin'] > $horarioobjetos[$k]['horarios'][$j]['inicio']) || ($horarioobjetos[$k]['horarios'][$j]['fin'] > $horarioobjetos[$k]['horarios'][$i]['inicio'])) {
+						$horarioobjetos[$k]['horarios'][$i]['solapado'] = true;
+						$horarioobjetos[$k]['horarios'][$j]['solapado'] = true;
+					}
+				}
+			}
+		}
+		//Eliminar horarios sin solape
+		foreach ($horarioobjetos as $k => $v) {
+			foreach ($v['horarios'] as $k2 => $v2) {
+				if (!$v2['solapado']) {
+					unset($horarioobjetos[$k]['horarios'][$k2]);
+				}
+			}
+		}
+		//Copiar sólo los que aún tengan algo en el horario
+		$solapados = array();
+		foreach ($horarioobjetos as $k => $v) {
+			if (count($v['horarios']) > 0) {
+				$solapados[$k] = $v;
+			}
+		}
+
+		$this->set('nosatzona', $nosatzona);
+		$this->set('nosatactividad', $nosatactividad);
+		$this->set('infraobjeto', $infraobjeto);
+		$this->set('multiobjeto', $multiobjeto);
+		$this->set('sobreasignacion', $sobreasignacion);
+		$this->set('activzona', $activzona);
+		$this->set('solapados', $solapados);
 	}
 }
---
+++ View/Generales/estado.ctp
@@ -0,0 +1,134 @@
+<?php
+echo $this->Html->script('jquery');
+?>
+
+<h1>Verificación de estado</h1>
+
+<h2 onclick="$('#nosatzona').toggle('slow')"><?php echo count($nosatzona); ?> necesidades de zona no satisfechas (+)</h2>
+<div id="nosatzona">
+	<table>
+		<tr><th>Zona</th><th>Necesidad</th><th>Cantidad</th></tr>
+<?php
+	foreach ($nosatzona as $v) {
+		echo "\t\t<tr><td>".$this->Html->link($v['Zona']['nombre'], array($v['Zona']['id'], 'action' => 'view', 'controller' => 'Zonas')).'</td><td>'.htmlspecialchars($v['Necesidadzona']['descripcion'])."</td><td>{$v['Necesidadzona']['cantidad']}</td></tr>\n";
+	}
+?>
+	</table>
+</div>
+
+<h2 onclick="$('#nosatactividad').toggle('slow')"><?php echo count($nosatactividad); ?> necesidades de actividad no satisfechas (+)</h2>
+<div id="nosatactividad">
+	<table>
+		<tr><th>Zona</th><th>Actividad</th><th>Necesidad</th><th>Cantidad</th></tr>
+<?php
+	foreach ($nosatactividad as $v) {
+		echo "\t\t<tr><td>".$this->Html->link($v['Zona']['nombre'], array($v['Zona']['id'], 'action' => 'view', 'controller' => 'Zonas')).'</td><td>'.$this->Html->link($v['Actividad']['nombre'], array($v['Actividad']['id'], 'action' => 'view', 'controller' => 'Actividades')).'</td><td>'.htmlspecialchars($v['Necesidadactividad']['descripcion'])."</td><td>{$v['Necesidadactividad']['cantidad']}</td></tr>\n";
+	}
+?>
+	</table>
+</div>
+
+<h2 onclick="$('#infraobjeto').toggle('slow')"><?php echo count($infraobjeto); ?> necesidades de infraestructura con objeto asignado (+)</h2>
+<div id="infraobjeto">
+	<table>
+		<tr><th>Dónde</th><th>Necesidad</th><th>Cantidad</th><th>Objeto</th></tr>
+<?php
+	foreach ($infraobjeto as $v) {
+		if ($v['idactividad'] == -1) {
+			$donde = 'Zona: '.$this->Html->link($v['nomzona'], array($v['idzona'], 'action' => 'view', 'controller' => 'Zonas'));
+		}
+		else {
+			$donde = 'Actividad: '.$this->Html->link($v['nomactividad'], array($v['idactividad'], 'action' => 'view', 'controller' => 'Actividades')).', en '.$this->Html->link($v['nomzona'], array($v['idzona'], 'action' => 'view', 'controller' => 'Zonas'));
+		}
+		echo "\t\t<tr><td>$donde</td><td>".htmlspecialchars($v['necesidad'])."</td><td>{$v['cantidad']}</td><td>".$this->Html->link($v['descobjeto'], array($v['idobjeto'], 'action' => 'view', 'controller' => 'Objetos'))."</td></tr>\n";
+	}
+?>
+	</table>
+</div>
+
+<h2 onclick="$('#multiobjeto').toggle('slow')"><?php echo count($multiobjeto); ?> objetos no fungibles con asignación múltiple (+)</h2>
+<div id="multiobjeto">
+	<table>
+		<tr><th>Dónde</th><th>Necesidad</th><th>Cantidad</th><th>Objeto</th></tr>
+<?php
+	foreach ($multiobjeto as $v) {
+		if ($v['idactividad'] == -1) {
+			$donde = 'Zona: '.$this->Html->link($v['nomzona'], array($v['idzona'], 'action' => 'view', 'controller' => 'Zonas'));
+		}
+		else {
+			$donde = 'Actividad: '.$this->Html->link($v['nomactividad'], array($v['idactividad'], 'action' => 'view', 'controller' => 'Actividades')).', en '.$this->Html->link($v['nomzona'], array($v['idzona'], 'action' => 'view', 'controller' => 'Zonas'));
+		}
+		echo "\t\t<tr><td>$donde</td><td>".htmlspecialchars($v['necesidad'])."</td><td>{$v['cantidad']}</td><td>".$this->Html->link($v['descobjeto'], array($v['idobjeto'], 'action' => 'view', 'controller' => 'Objetos'))."</td></tr>\n";
+	}
+?>
+	</table>
+</div>
+
+<h2 onclick="$('#sobreasignacion').toggle('slow')"><?php echo count($sobreasignacion); ?> objetos con sobreasignacion (+)</h2>
+<div id="sobreasignacion">
+	<table>
+		<tr><th>Objeto</th><th>Disponibles</th><th>Necesarios</th></tr>
+<?php
+	foreach ($sobreasignacion as $v) {
+		echo "\t\t<tr><td>".$this->Html->link($v['descobjeto'], array($v['idobjeto'], 'action' => 'view', 'controller' => 'Objetos'))."</td><td>{$v['disponible']}</td><td>{$v['uso']}</td></tr>\n";
+	}
+?>
+	</table>
+</div>
+
+<h2 onclick="$('#activzona').toggle('slow')"><?php echo count($activzona); ?> objetos no fungibles asignados a zona y actividad simultáneamente (+)</h2>
+<div id="activzona">
+	<table>
+		<tr><th>Objeto</th><th>Zonas</th><th>Actividades</th></tr>
+<?php
+	foreach ($activzona as $k => $v) {
+		echo "\t\t<tr><td>".$this->Html->link($v['descripcion'], array($k, 'action' => 'view', 'controller' => 'Objetos'))."</td>\n<td><ul>";
+		foreach ($v['zonas'] as $k2 => $v2) {
+			echo '<li>'.$this->Html->link($v2, array($k2, 'action' => 'view', 'controller' => 'Zonas')).'</li>';
+		}
+		echo "</ul></td>\n<td>";
+		foreach ($v['actividades'] as $k2 => $v2) {
+			echo '<li>'.$this->Html->link($v2, array($k2, 'action' => 'view', 'controller' => 'Actividades')).'</li>';
+		}
+		echo "</ul></td></tr>\n";
+	}
+?>
+	</table>
+</div>
+
+<h2 onclick="$('#solapados').toggle('slow')"><?php echo count($activzona); ?> objetos no fungibles con solape en actividades (+)</h2>
+<div id="solapados">
+	<table>
+		<tr><th>Objeto</th><th>Actividad</th><th>Inicio</th><th>Fin</th></tr>
+<?php
+	foreach ($solapados as $k => $v) {
+		$numhorarios = count($v['horarios']);
+		$primero = array_pop($v['horarios']);
+		echo "\t\t<tr><td rowspan=\"$numhorarios\">".$this->Html->link($v['descobjeto'], array($k, 'action' => 'view', 'controller' => 'Objetos'))."</td>\n";
+		echo "\t\t\t<td>".$this->Html->link($primero['nomactividad'], array($primero['idactividad'], 'action' => 'view', 'controller' => 'Actividades'))."</td>\n";
+		echo "\t\t\t<td>{$primero['inicio']}</td>\n";
+		echo "\t\t\t<td>{$primero['fin']}</td>\n";
+		echo "\t\t</tr>\n";
+		foreach ($v['horarios'] as $v2) {
+			echo "\t\t\t<tr>\n";
+			echo "\t\t\t<td>".$this->Html->link($v2['nomactividad'], array($v2['idactividad'], 'action' => 'view', 'controller' => 'Actividades'))."</td>\n";
+			echo "\t\t\t<td>{$v2['inicio']}</td>\n";
+			echo "\t\t\t<td>{$v2['fin']}</td></tr>\n";
+		}
+	}
+?>
+	</table>
+</div>
+<?php
+	echo "<p>".$this->Html->link('Volver al inicio', '/')."</p>";
+
+	echo $this->Html->scriptBlock('
+		$(\'#nosatzona\').hide();
+		$(\'#nosatactividad\').hide();
+		$(\'#infraobjeto\').hide();
+		$(\'#multiobjeto\').hide();
+		$(\'#sobreasignacion\').hide();
+		$(\'#activzona\').hide();
+		$(\'#solapados\').hide();
+	');
+?>
